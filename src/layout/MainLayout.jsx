4ffbacbbf743bc8c5548be2a1d8c135409e8dc2e
GmMamunH <gmmamun3004@gmail.com>
2025-01-18 17:08:25 +0600

    add(home): add home page

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/header/navbar/navbar";
 
 export const MainLayout = () => {
   return (
     <div>
       <Navbar />
+      <div>
+        <Outlet/>
+      </div>
     </div>
   );
 };
